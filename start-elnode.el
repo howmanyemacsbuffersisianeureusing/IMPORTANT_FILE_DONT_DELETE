da68d8ce27a30349fcff4a31d0b7c80522b45831
Allen Goodman <allen.goodman@me.com>
2012-12-05 14:25:27 -0800

    Fixed egregious style issues.

--- start-elnode.el
+++ start-elnode.el
@@ -2,18 +2,16 @@
 (setq package-archives '(("marmalade" . "http://marmalade-repo.org/packages/")))
 (package-initialize)
 (package-refresh-contents)
+
 (package-install 'elnode)
 
 (defun handler (httpcon)
   "How many Emacs buffers is Ian Eure using?"
-  (elnode-http-start httpcon "200"
+  (elnode-http-start httpcon 200
                      '("Content-type" . "text/html")
                      `("Server" . ,(concat "GNU Emacs " emacs-version)))
-  (elnode-http-return httpcon "<html><body><h1>All of them.</h1></body></html>"))
+  (elnode-http-return httpcon "<html><b>All of them.</b></html>"))
 
-(elnode-start 
-    'handler 
-    :port (string-to-number (or (getenv "PORT") "8080")) 
-    :host "0.0.0.0")
+(elnode-start 'handler :port 80 :host "localhost")
 
 (while t (accept-process-output nil 1))
